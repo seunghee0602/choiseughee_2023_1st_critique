612215e8d3579d2f7bdad498d73f777fa8582d7d
seunghee0602 <143109450+seunghee0602@users.noreply.github.com>
2023-09-04 23:30:57 +0900

    1차 심사 업로드

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,16 @@
+const reference = [
+  {
+    author: "유순덕",
+    title: "인공지능 비니 생태계 연구",
+    year: "2020",
+    sourceTitle: "",
+    link: "http://koreascience.or.kr/article/JAKO202014151761484.page",
+  },
+  {
+    author: "조진형, 김규정",
+    title: "소셜미디어에서 에코챔버에 의한 필터버블 현상 개선 방안 연구",
+    year: "2022",
+    sourceTitle: "",
+    link: "http://koreascience.or.kr/article/JAKO202216855740546.page",
+  },
+];
---
+++ src/subject3/script/reference.js
@@ -0,0 +1,25 @@
+const reference = [
+  {
+    author: "배우리",
+    title: "또 하나의 생활문화 지도 땅이름",
+    year: "2023",
+    sourceTitle: "",
+    link: "",
+  },
+
+  {
+    author: "김동실",
+    title: "서울의 시가지 확대와 지형적 배경",
+    year: "2008",
+    sourceTitle: "",
+    link: "https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE02494661",
+  },
+
+  {
+    author: "나각순",
+    title: "서울지명의 변천과 특징",
+    year: "2008",
+    sourceTitle: "",
+    link: "https://www.kci.go.kr/kciportal/ci/sereArticleSearch/ciSereArtiView.kci?sereArticleSearchBean.artiId=ART001289698",
+  },
+];
